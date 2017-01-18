3f9f42bb320ab75719da0b5f3985aa3fc403f14a
asbis92 <rdsl_ivanov@yahoo.com>
2017-01-18 13:55:13 +0200

    Program that calculates the area and perimeter of a circle

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Area_and_Perimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+            Console.Write("Area = ");
+            Console.WriteLine(Area(radius));
+            Console.Write("Perimeter = ");
+            Console.WriteLine(Perimeter(radius));
+
+        } 
+        static double Area (double r)
+        {
+            double area = Math.PI * r * r;
+            return area;
+        } 
+        static double Perimeter (double r)
+        {
+            double perimeter = 2 * Math.PI * r;
+            return perimeter;
+        }
+    }
+}
